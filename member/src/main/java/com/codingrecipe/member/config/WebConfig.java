888f3da9a68f9f78c9015b837a4f56c79889814e
Kyungjin <sarahkj45@gmail.com>
2023-11-25 22:56:54 +0900

    11.23

--- member/src/main/java/com/codingrecipe/member/config/WebConfig.java
+++ member/src/main/java/com/codingrecipe/member/config/WebConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    private String resourcePath = "/upload/**"; // view 에서 접근할 경로
+    private String resourcePath = "/upload/**"; // view 에서 접근할 경로!
     private String savePath = "file:///C:/Projects/springboot_img/"; // 실제 파일 저장 경로(win)
 
     @Override
